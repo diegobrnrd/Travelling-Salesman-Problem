8da9aeba878bbdd9713019113e700b1e73d148e1
Diego Bernardo <diegobsiufrpe@gmail.com>
2024-08-27 19:27:59 -0300

    knn

--- knn.py
+++ knn.py
@@ -1,50 +1,61 @@
-from random import randint
-
 def ler_arquivo():
+    """Coleta as coordenadas e as armazena em um dicionário."""
     with open('berlin52.tsp', 'r') as arquivo:
         coordenadas = {}
         for linha in arquivo.readlines()[6:58]:
             cidade, x, y = linha.strip().split()
             coordenadas[int(cidade)] = (float(x), float(y))
+
     return coordenadas
 
 
 def vizinho_mais_proximo(coordenadas):
-    cidade_inicial = randint(1, len(coordenadas))
-    percurso = [-1] * (len(coordenadas) + 1)
-    menor_distancia_encontrada = 0
-    cidades_nao_visitadas = list(coordenadas.keys())
+    """Implementa o algoritmo do vizinho mais próximo."""
+    cidades = list(coordenadas.keys())
+    cidade_inicial = choice(cidades)
     cidade_atual = cidade_inicial
 
-    for k in range(len(coordenadas) - 1):
+    percurso = [-1] * (len(coordenadas) + 1)
+    percurso[0] = cidade_atual
+    distancia_total = 0
+
+    # Itera sobre o número de cidades menos uma vez, pois a cidade inicial já está no percurso.
+    for k in range(1, len(coordenadas)):
         cidade_mais_proxima = None
         menor_distancia = float("inf")
-        cidades_nao_visitadas.remove(cidade_atual)
 
-        for cidade in cidades_nao_visitadas:
+        # Remove a cidade atual da lista de cidades a serem visitadas.
+        cidades.remove(cidade_atual)
+
+        # Calcula a distância para cada cidade não visitada e encontra a mais próxima.
+        for cidade in cidades:
             x1, y1 = coordenadas[cidade_atual]
             x2, y2 = coordenadas[cidade]
             distancia = calcular_distancia(x1, y1, x2, y2)
 
+            # Atualiza a cidade mais próxima e a menor distância encontrada.
             if distancia < menor_distancia:
                 cidade_mais_proxima = cidade
                 menor_distancia = distancia
 
+        # Adiciona a cidade mais próxima ao percurso e atualiza a distância total.
         percurso[k] = cidade_mais_proxima
-        menor_distancia_encontrada += menor_distancia
-        percurso[len(coordenadas) - 1] = cidade_atual
+        distancia_total += menor_distancia
+
+        # Define a cidade mais próxima como a cidade atual para a próxima iteração.
         cidade_atual = cidade_mais_proxima
 
+    # Calcula a distância para voltar à cidade inicial.
     x1, y1 = coordenadas[cidade_atual]
     x2, y2 = coordenadas[cidade_inicial]
-    distancia_para_inicial = calcular_distancia(x1, y1, x2, y2)
-    percurso[len(coordenadas)] = -1
-    menor_distancia_encontrada += distancia_para_inicial
+    distancia_total += calcular_distancia(x1, y1, x2, y2)
 
-    return percurso, menor_distancia_encontrada
+    # Retorna o percurso completo e a distância total percorrida.
+    return percurso, distancia_total
 
 
 def calcular_distancia(x1, y1, x2, y2):
+    """Calcula a distância euclidiana entre dois pontos no plano cartesiano."""
     a = (x2 - x1) ** 2
     b = (y2 - y1) ** 2
     x = a + b
@@ -52,20 +63,26 @@ def calcular_distancia(x1, y1, x2, y2):
 
 
 def raiz_quadrada(x, x0, e):
+    """Calcula a raiz quadrada."""
     if abs(x0 ** 2 - x) <= e:
         return x0
     else:
         return raiz_quadrada(x, ((x0 ** 2 + x) / (2 * x0)), e)
 
 
-def funcao_principal():
+def funcao_central():
     coordenadas = ler_arquivo()
     percurso, distancia = vizinho_mais_proximo(coordenadas)
 
     for c in percurso:
         print(c)
-    print(int(distancia))
+
+    print(f'\n{int(distancia)}')
 
 
 if __name__ == '__main__':
-    funcao_principal()
+    """Cronometra o tempo necessário para a execução do algoritmo."""
+    from random import choice
+    import timeit
+    tempo_de_execucao = timeit.timeit(funcao_central, number=1)
+    print(f'\nTempo de execução: {tempo_de_execucao:.6f} segundos.')
